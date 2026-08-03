d263beddddc3a4310f0b7c25386db063c62cf638
BostHost <lopezdavison7@gmail.com>
2026-08-03 09:39:57 -0600

    Crear ping.js

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+export default {
+    nombre: 'ping',
+    alias: [],
+    descripcion: 'Muestra la velocidad de respuesta del bot',
+    ejecutar: async ({ responder, msg }) => {
+        const inicio = Date.now();
+        const marcaMensaje = (msg.messageTimestamp ? Number(msg.messageTimestamp) * 1000 : inicio);
+        const latencia = Math.max(inicio - marcaMensaje, 0);
+        await responder.texto(`🏓 *Pong!*\nLatencia: ${latencia}ms`);
+    }
+};
