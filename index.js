d4b3dba98d6903be2f2f24a6d3b211702ae6f38c
BostHost <lopezdavison7@gmail.com>
2026-08-04 13:33:24 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import baileysPkg from '@whiskeysockets/baileys';
-const { makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = baileysPkg;
+import makeWASocket, { useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion } from '@whiskeysockets/baileys';
 import { Boom } from '@hapi/boom';
 import Fastify from 'fastify';
 import pino from 'pino';
