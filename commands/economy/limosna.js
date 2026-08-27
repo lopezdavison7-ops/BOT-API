8c7423102f75141c5801c82002e87db038879007
BostHost <lopezdavison7@gmail.com>
2026-08-27 13:55:09 -0600

    Crear limosna.js

---
+++ commands/economy/limosna.js
@@ -0,0 +1,18 @@
+import { addDinero } from '../../lib/economia.js';
+
+export default {
+    nombre: 'limosna',
+    categoria: 'economia',
+    descripcion: 'Pide limosna. 1 vez cada 12h.limosna',
+    cooldown: 43200000, // 12 horas
+    ejecutar: async ({ sock, msg, jid }) => {
+        const jugador = msg.key.participant || msg.key.remoteJid;
+        const premio = Math.floor(Math.random() * 91) + 10; // 10 a 100
+
+        addDinero(jugador, premio);
+
+        await sock.sendMessage(jid, {
+            text: `🙏 *LIMOSNA RECIBIDA* 🙏\n\nAlguien te dio ${premio} 🪙 por lástima\nCartera: ${getDinero(jugador)} 🪙`
+        });
+    }
+}
