8e84f419ad656f5fce8c7949742560fb3b48f822
BostHost <lopezdavison7@gmail.com>
2026-08-31 17:57:18 -0600

    Actualizar ss.js

--- commands/downloads/ss.js
+++ commands/downloads/ss.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import config from '../../config.js';
 
 const API_URL =
-    'https://apiyosoyyo-ofc.onrender.com/api/ssweb';
+    'https://api-yosoyyo-api-ofc.onrender.com/api/ssweb';
 
 export default {
     nombre: 'ss',
